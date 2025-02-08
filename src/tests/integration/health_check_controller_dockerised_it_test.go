7345cd3020d3e3eb6d1057a5f3cbee2958449075
Toms Druvaskalns <tomsdruvas@gmail.com>
2025-02-08 15:57:43 +0000

    add body response assertions to health check containerised test

--- src/tests/integration/health_check_controller_dockerised_it_test.go
+++ src/tests/integration/health_check_controller_dockerised_it_test.go
@@ -1,7 +1,9 @@
 package controllers_test
 
 import (
+	"bytes"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http"
 	"os"
 	. "src/src/tests/integration/setup"
@@ -20,5 +22,13 @@ func TestMain(m *testing.M) {
 func TestHealthCheckIntegration(t *testing.T) {
 	resp, err := http.Get(testServerURL + "/health")
 	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	bodyString := string(bytes.TrimSpace(bodyBytes))
+	expectedBody := `{"message":"Service is up and running","status":"healthy"}`
+
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.JSONEq(t, expectedBody, bodyString, "Unexpected response body")
 }
