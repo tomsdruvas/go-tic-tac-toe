ad78be4940a2298920a601246ff29f6e09979467
tomsdruvas <93099357+tomsdruvas@users.noreply.github.com>
2025-02-17 17:02:10 +0000

    Update create_game_controller_dockerised_it_test.go
    add tests for containerised retrieval of game session

--- src/tests/integration/create_game_controller_dockerised_it_test.go
+++ src/tests/integration/create_game_controller_dockerised_it_test.go
@@ -10,7 +10,29 @@ import (
 	"testing"
 )
 
-func TestCreateGameIntegration(t *testing.T) {
+var currentSessionId string
+
+func TestTicTacToeFullFlow(t *testing.T) {
+	createGameSessionIntegration(t)
+	retrieveGameSessionIntegration(t)
+}
+
+func TestRetrieveNotFoundGameSessionIntegration(t *testing.T) {
+	resp, err := http.Get(TestServerURL + "/game-session/1234")
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	var response map[string]interface{}
+	err = json.Unmarshal(bodyBytes, &response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func createGameSessionIntegration(t *testing.T) {
 	jsonBody := `{"player1": "John"}`
 	bodyReader := strings.NewReader(jsonBody)
 	resp, err := http.Post(TestServerURL+"/game-session", "application/json", bodyReader)
@@ -27,8 +49,39 @@ func TestCreateGameIntegration(t *testing.T) {
 	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 
 	sessionId, ok := response["sessionId"].(string)
-	assert.True(t, ok, "sessionId should be a string")
-	assert.NotEmpty(t, sessionId, "sessionId should not be empty")
+	assert.True(t, ok)
+	assert.NotEmpty(t, sessionId)
+	currentSessionId = sessionId
+
+	assert.Equal(t, "John", response["player1"])
+	assert.Equal(t, "John", response["nextPlayerMove"])
+
+	expectedGrid := []interface{}{
+		[]interface{}{float64(0), float64(0), float64(0)},
+		[]interface{}{float64(0), float64(0), float64(0)},
+		[]interface{}{float64(0), float64(0), float64(0)},
+	}
+	assert.Equal(t, expectedGrid, response["gameGrid"])
+}
+
+func retrieveGameSessionIntegration(t *testing.T) {
+	resp, err := http.Get(TestServerURL + "/game-session/" + currentSessionId)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	var response map[string]interface{}
+	err = json.Unmarshal(bodyBytes, &response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	sessionId, ok := response["sessionId"].(string)
+	assert.True(t, ok)
+	assert.NotEmpty(t, sessionId)
+	currentSessionId = sessionId
 
 	assert.Equal(t, "John", response["player1"])
 	assert.Equal(t, "John", response["nextPlayerMove"])
